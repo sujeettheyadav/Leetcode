560d4b89dd377b9da75ce5e3b6d8e830581f2536
sujeettheyadav <sujeettheyadav@gmail.com>
2026-09-25 21:41:05 +0530

    Time: 2 ms (59.36%), Space: 43 MB (66.68%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> stack1=new Stack<>();
+        Stack<Character> stack2=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='#'){
+                if(!stack1.isEmpty()){
+                    stack1.pop();
+                }
+            }
+            else{
+                stack1.push(c);
+            }
+        }
+        for(char ch:t.toCharArray()){
+            if(ch=='#'){
+                if(!stack2.isEmpty()){
+                    stack2.pop();
+                }
+            }
+            else{
+                stack2.push(ch);
+            }
+        }
+        return stack1.equals(stack2);   
+    }
+}
